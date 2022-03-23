d6b948b9ff4a5e2880b5166c82a3034172e6b21b
ChoBaeKR <tmsprqo@gmail.com>
2022-03-23 23:35:37 +0900

    22.03.23 backjoon

--- codingtest/backjoon/1700.py
+++ codingtest/backjoon/1700.py
@@ -18,15 +18,18 @@ for i in range(k):
     else:
         # 멀티탭
         for mac in tmp:
+            # 멀티탭이 후에 사용되지 않으면 바로 인덱스 처리해주고 브레이크
             if mac not in machineLi[i:]:
                 idx = mac
                 break
-            #
+            # 멀티탭이 후에 사용될때 가장 뒤에 사용하는 것을 빼준다.
             elif machineLi[i:].index(mac) > maxIdx:
                 maxIdx = machineLi[i:].index(mac)
                 idx = mac
 
+        # 멀티탭에서 뽑아주고 현재 기기를 꼽아준다.
         tmp[tmp.index(idx)] = machineLi[i]
+        # 멀티탭 뽑은횟수 증가.
         answer += 1
     
 print(answer)        

13f7b95aefc9896d0beff6fbcc095a2cab00ea0d
ChoBaeKR <tmsprqo@gmail.com>
2022-03-28 19:24:30 +0900

    22.03.28 지출비용 차트 정리 추가

---
+++ ReactCompleteGuideUdemy/ReactStart/src/components/Chart/ChartBar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./ChartBar.css";
+
+const ChartBar = (props) => {
+  let barFillHeight = "0%";
+
+  if (props.maxValue > 0) {
+    barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
+  }
+  return (
+    <div className="chart-bar">
+      <div className="chart-bar__inner">
+        {/* {{}} 두개의 중괄호를 사용해서 html 형식에 값을 넣게해준다. */}
+        <div
+          className="chart-bar__fill"
+          style={{ height: barFillHeight }}
+        ></div>
+      </div>
+      <div className="chart-bar__label">{props.label}</div>
+    </div>
+  );
+};
+
+export default ChartBar;

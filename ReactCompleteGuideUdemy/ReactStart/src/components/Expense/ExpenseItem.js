89a7281fd1ff8943b122edc0e395d4aea951c799
ChoBaeKR <tmsprqo@gmail.com>
2022-03-28 19:31:36 +0900

    22.03.28 ul을 사용해서 li로커버해줌

--- ReactCompleteGuideUdemy/ReactStart/src/components/Expense/ExpenseItem.js
+++ ReactCompleteGuideUdemy/ReactStart/src/components/Expense/ExpenseItem.js
@@ -1,18 +1,20 @@
 import ExpenseDate from "./ExpenseDate";
-import Card from "../UI/Card"
+import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
   return (
-    <Card className="expense-item">
-      {/* {}를 사용하면 자바스크립트를 사용가능 */}
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">{props.amount}원</div>
-      </div>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        {/* {}를 사용하면 자바스크립트를 사용가능 */}
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">{props.amount}원</div>
+        </div>
+      </Card>
+    </li>
   );
-}
+};
 
 export default ExpenseItem;

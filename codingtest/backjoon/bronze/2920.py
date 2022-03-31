aeb8f2770d745f6850f3d8564de11b587c627c3c
ChoBaeKR <101175828+ChoBaeKR@users.noreply.github.com>
2022-03-31 16:42:41 +0900

    리포지토리 이동

--- codingtest/backjoon/bronze/2920.py
+++
@@ -1,9 +0,0 @@
-# 음계
-numLi = list(map(int, input().split()))
-
-if numLi == sorted(numLi):
-    print('ascending')
-elif numLi == sorted(numLi,reverse= True):
-    print('descending')
-else:
-    print('mixed')

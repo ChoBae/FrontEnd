f02909d781cc73cb16eac8f0ad2feee1e8664f30
ChoBaeKR <101175828+ChoBaeKR@users.noreply.github.com>
2022-04-11 18:18:56 +0900

    22.04.11 음식 주문 앱 초기 세팅

---
+++ ReactCompleteGuideUdemy/FoodOrder/src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import { Fragment } from "react";
+import classes from "./Header.module.css"
+import mealImage from "../../assets/meals.jpg"
+import HeaderCartButton from "./HeaderCartButton";
+const Header = () => {
+    return(
+        <Fragment>
+            <header className={classes.header}>
+                <h1>리액트의 민족</h1>
+                <HeaderCartButton/>
+            </header>
+            {/* '-'은 .으로 처리할수없어서 대괄호로 처리함 */}
+            <div className={classes[`main-image`]}>
+                <img src={mealImage} alt="음식이 한상 차려져 있는 사진" ></img>
+            </div>
+        </Fragment>
+    );
+};
+
+export default Header;
---
+++ ReactCompleteGuideUdemy/FoodOrder/src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,16 @@
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+
+const HeaderCartButton = (props) => {
+  return (
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon></CartIcon>
+      </span>
+      <span>내 카트</span>
+      <span className={classes.badge}> 3</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;

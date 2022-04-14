6a6705c7a25e3b3e940196a39f82e409f105ac0b
ChoBaeKR <101175828+ChoBaeKR@users.noreply.github.com>
2022-04-14 17:32:32 +0900

    22.04.14 ref 적용

--- ReactCompleteGuideUdemy/FoodOrder/src/components/Meals/MealItem/MealItemForm.js
+++ ReactCompleteGuideUdemy/FoodOrder/src/components/Meals/MealItem/MealItemForm.js
@@ -1,13 +1,35 @@
+import { useRef, useState } from "react";
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber);
+  };
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="수량"
         input={{
-          id: "amount",
+          id: "amount_" + props.id,
           type: "number",
           min: "1",
           max: "5",
@@ -16,9 +38,9 @@ const MealItemForm = (props) => {
         }}
       ></Input>
       <button>+ 추가하기</button>
+      {!amountIsValid && <p> 가능한 범위의 수를 입력해주세요 (1~5) </p>}
     </form>
   );
 };
 
 export default MealItemForm;
- 

b3cf9869ec9df4d447fa3d41dd9d8e83f6a3cf2d
ChoBaeKR <tmsprqo@gmail.com>
2022-04-14 12:47:22 +0900

    22.04.13 음식 추가 컴포넌트 추가

---
+++ ReactCompleteGuideUdemy/FoodOrder/src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,24 @@
+import Input from "../../UI/Input";
+import classes from "./MealItemForm.module.css";
+
+const MealItemForm = (props) => {
+  return (
+    <form className={classes.form}>
+      <Input
+        label="수량"
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      ></Input>
+      <button>+ 추가하기</button>
+    </form>
+  );
+};
+
+export default MealItemForm;
+ 

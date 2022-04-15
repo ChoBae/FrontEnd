f8739dec694a2386ac8d478e5b2c75af5a5542c2
ChoBaeKR <101175828+ChoBaeKR@users.noreply.github.com>
2022-04-15 15:56:44 +0900

    22.04.15 한화 전환

--- ReactCompleteGuideUdemy/FoodOrder/src/components/Meals/MealItem/MealItem.js
+++ ReactCompleteGuideUdemy/FoodOrder/src/components/Meals/MealItem/MealItem.js
@@ -6,7 +6,7 @@ import MealItemForm from "./MealItemForm";
 const MealItem = (props) => {
   const cartCtx = useContext(CartContext);
 
-  const price = `${props.price.toFixed(2)}원`;
+  const price = `${props.price}원`;
   const addToCartHandler = (amount) => {
     cartCtx.addItem({
       id: props.id,

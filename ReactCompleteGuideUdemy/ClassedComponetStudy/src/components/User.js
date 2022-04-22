87c30e1af2380a77e85804b6b14812caab330b70
ChoBaeKR <tmsprqo@gmail.com>
2022-04-22 23:39:49 +0900

    22.04.22 클래스형 컴포넌트에서 useEffect와 같은 기능 다루기

--- ReactCompleteGuideUdemy/ClassedComponetStudy/src/components/User.js
+++ ReactCompleteGuideUdemy/ClassedComponetStudy/src/components/User.js
@@ -1,7 +1,17 @@
-import classes from './User.module.css';
-
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+import { Component } from "react";
+import classes from "./User.module.css";
+// 클래스형 컴포넌트 -> 요새는 함수형을 쓴다. 확실히 함수형이 편한 느낌 -> props를 줄때 this로 예약해줘야함.
+class User extends Component {
+  // useEffect와 같은 기능 -> 클린업 함수 -> 끝날때 마다 실행
+  componentWillUnmount(){
+    console.log("함수형 컴포넌트의 useEffect의 클린업과 비슷해!")
+  }
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;

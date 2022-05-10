3d4699c3f027ba9573ead000431a5e14805a6691
ChoBaeKR <101175828+ChoBaeKR@users.noreply.github.com>
2022-05-10 17:53:17 +0900

    22.05.10 커스텀훅 http 적용

--- ReactCompleteGuideUdemy/customHooksToBackend/src/App.js
+++ ReactCompleteGuideUdemy/customHooksToBackend/src/App.js
@@ -1,43 +1,33 @@
-import React, { useEffect, useState } from 'react';
-
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
+import React, { useEffect, useState} from "react";
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
+  // 커스텀훅으로 받는 데이터 처리
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-9ba90-default-rtdb.firebaseio.com/tasks.json'
-      );
+  // 커스텀 훅 사용
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
+  useEffect(() => {
+    const transformTasks = (tasksObj) => {
       const loadedTasks = [];
 
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
       }
 
       setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
+    };
 
-  useEffect(() => {
-    fetchTasks();
-  }, []);
+    fetchTasks(
+      {
+        url: "https://react-http-9ba90-default-rtdb.firebaseio.com/tasks.json",
+      },
+      transformTasks
+    );
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
--- ReactCompleteGuideUdemy/customHooksToBackend/src/hooks/use-http.js
+++ ReactCompleteGuideUdemy/customHooksToBackend/src/hooks/use-http.js
@@ -1,41 +1,38 @@
-import React, { useState } from "react";
-const useHttp = (requestConfig, applyData) => {
+import { useState, useCallback } from "react";
+
+// http 커스텀훅
+const useHttp = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const sendRequest = async () => {
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
     setIsLoading(true);
     setError(null);
+    // 디폴트값을 GET으로 설정(값 받아오는것)
     try {
-      const response = await fetch(
-        requestConfig.url, {
-            method: requestConfig.method,
-            headers: requestConfig.headers,
-            body: JSON.stringify(requestConfig.body)
-        }
-      );
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : "GET",
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+      });
 
       if (!response.ok) {
         throw new Error("Request failed!");
       }
 
       const data = await response.json();
-      applyData(data)
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
+      // 컴포넌트단에서 함수 실행?
+      applyData(data);
     } catch (err) {
       setError(err.message || "Something went wrong!");
     }
     setIsLoading(false);
-  };
-
+  }, []);
+  // 해당값들 넘겨주기
   return {
-      isLoading,
-      error,
-      sendRequest
-  }
+    isLoading,
+    error,
+    sendRequest,
+  };
 };
 export default useHttp;

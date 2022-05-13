e2de675fb65b891f5ca5bfb97e5e0e7aa78bc793
ChoBaeKR <101175828+ChoBaeKR@users.noreply.github.com>
2022-05-13 16:54:07 +0900

    22.05.13 task 추가 기능 중복 로직 커스텀훅 전환

--- ReactCompleteGuideUdemy/customHooksToBackend/src/components/NewTask/NewTask.js
+++ ReactCompleteGuideUdemy/customHooksToBackend/src/components/NewTask/NewTask.js
@@ -1,43 +1,35 @@
-import { useState } from 'react';
+import useHttp from "../../hooks/use-http";
 
-import Section from '../UI/Section';
-import TaskForm from './TaskForm';
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-9ba90-default-rtdb.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
+  // 커스텀 훅 사용 -> 같은 로직을 효과적으로 줄여줄 수 있다.
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
+  // 새로운 일 추가 기능
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+    props.onAddTask(createdTask);
   };
 
+  const enterTaskHandler = async (taskText) => {
+    // 2개의 파라미터값을 갖는 커스텀 훅 내의 기능
+    // task를 추가하는 것이라서 post와 post시 주어져야하는 것을을 객체로 보내주고, 두번째 파라미터값으로 task를 추가하는 함수가 실행된다. 
+    // bind는 보통 변수에 다른 함수를 불러오려할때 가져오지 못하는데 그 경우 bind를 써서 불러오게 함
+    // 파라미터값으로 데이터를 주어줘야할때도 사용하는듯?
+    sendTaskRequest(
+      {
+        url: "https://react-http-9ba90-default-rtdb.firebaseio.com/tasks.json",
+        method: "POST",
+        body: { text: taskText },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      createTask.bind(null, taskText)
+    );
+  };
   return (
     <Section>
       <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
